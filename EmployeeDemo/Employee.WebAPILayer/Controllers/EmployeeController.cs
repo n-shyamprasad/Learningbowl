1b8ca242f86bce65086f6ef6077a6372f8ec6ac8
Shyamprasad <n.shyamprasad@hotmail.com>
2020-04-10 07:15:24 +0800

    added few employees to the list
    in webapi controller added few more employees to the list

--- EmployeeDemo/Employee.WebAPILayer/Controllers/EmployeeController.cs
+++ EmployeeDemo/Employee.WebAPILayer/Controllers/EmployeeController.cs
@@ -19,6 +19,11 @@ namespace Emp.WebAPILayer.Controllers
             employees.Add(new Employee { Id = 101, EmpName = "John K", EmpMobile = "+001 1234567", EmpAddress = "Los Angeles, USA" });
             employees.Add(new Employee { Id = 201, EmpName = "David J", EmpMobile = "+65 8234 5678", EmpAddress = "Raffles, Singapore" });
             employees.Add(new Employee { Id = 301, EmpName = "N S Prasad", EmpMobile = "+91 94401 25678", EmpAddress = "Bangalore, India" });
+            employees.Add(new Employee { Id = 302, EmpName = "Mahesh M", EmpMobile = "+91 98355 25558", EmpAddress = "Hyderabad, India" });
+            employees.Add(new Employee { Id = 303, EmpName = "K H Kumaran", EmpMobile = "+91 98801 25999", EmpAddress = "Chennai, India" });
+            employees.Add(new Employee { Id = 202, EmpName = "Lee L", EmpMobile = "+65 7354 9870", EmpAddress = "Marinabay, Singapore" });
+            employees.Add(new Employee { Id = 102, EmpName = "Fedric OJ", EmpMobile = "+001 3478434612", EmpAddress = "NYC, USA" });
+            employees.Add(new Employee { Id = 301, EmpName = "Dickson MI", EmpMobile = "+001 998574247", EmpAddress = "NJ, USA" });
         }
 
         public IEnumerable<Employee> Get()
