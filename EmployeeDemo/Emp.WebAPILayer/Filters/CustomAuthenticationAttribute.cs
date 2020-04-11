87b5d48cb1480f3bfbad963f80bc0a0a3a865515
Shyamprasad <n.shyamprasad@hotmail.com>
2020-04-11 15:35:04 +0800

    Implemetned Bearer type authentication
    Implemetned Bearer type authentication
    and renamed some folders from Employee to Emp

---
+++ EmployeeDemo/Emp.WebAPILayer/Filters/CustomAuthenticationAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Security.Principal;
+using System.Net.Http;
+using System.Net;
+
+namespace Emp.WebAPILayer.Filters
+{
+    public class CustomAuthenticationAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization != null)
+            {
+                var authToken = actionContext.Request.Headers.Authorization.Parameter;
+                var decodeauthToken = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(authToken));
+                var vUsrPwd = decodeauthToken.Split(':');
+                if (IsAuthorizedUser(vUsrPwd[0], vUsrPwd[1]))
+                {
+                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(vUsrPwd[0]), null);
+                }
+                else
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+             }
+            else
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+        }
+
+        private bool IsAuthorizedUser(string pusr, string ppwd)
+        {
+            return pusr == "admin" && ppwd == "test";
+        }
+    }
+}
